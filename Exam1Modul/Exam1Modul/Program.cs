f918e4475c64145d22bd692c830f0de8b330e8ab
Shoxrux6616 <shoxruxsoliqulov@gmail.com>
2025-01-07 07:15:44 +0500

    rev4i9

--- Exam1Modul/Exam1Modul/Program.cs
+++ Exam1Modul/Exam1Modul/Program.cs
@@ -4,53 +4,6 @@ internal class Program
 {
     static void Main(string[] args)
     {
-
-        
+        Console.WriteLine("Hello World");
     }
-
-    /*// 1-misol
-    public static bool StartEndTrueFalse(string text)
-    {
-        if(text.StartsWith("crud") && text.EndsWith("crud"))
-        {
-            return true;
-        }
-            return false;
-    }*/
-
-    /*// 2-misol
-    public static int SumOfEvenElements(List<int> number)
-    {
-        var total = 0;
-        for (int i = 100; i < 1000; i++)
-        {
-            if (number[i] %2 == 0)
-            {
-                number[i] += total;
-            }
-        }
-        return total;
-    }*/
-
-    /*// 3-misol
-    public static bool StartEndSmuleText(List<char> text)
-    {
-        text.ToString();
-        if (text[0] >= 'a' && text[0] <= 'z' && text[text.Count -1] == 'a' && text[text.Count - 1] == 'z')
-        {
-            return true;
-        }
-        return false;
-    }*/
-
-    /*// 4-misol
-    public static string StartEndRemove(string text)
-    {
-        if (text.Length > 4)
-        {
-            text.Remove(0, 2);
-            text.Remove(text.Length - 2, 2);
-        }
-        return text;
-    }*/
 }
